4700c1c0f19dc4443d9ab5df3c7d6b90101beaac
박종훈 <m05214@naver.com>
2023-05-02 23:42:20 +0900

    Feat: 최초 프로젝트 등록
    최초 프로젝트 등록

---
+++ EffectiveJavaStudy/src/main/java/me/timonpark/ejs/effectivejavastudy/EffectiveJavaStudyApplication.java
@@ -0,0 +1,13 @@
+package me.timonpark.ejs.effectivejavastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveJavaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveJavaStudyApplication.class, args);
+    }
+
+}
