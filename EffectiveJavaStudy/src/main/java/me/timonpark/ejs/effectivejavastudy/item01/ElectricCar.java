d141df50eadcc7e3e616ea8b95b614ccb51bd071
박종훈 <m05214@marketboro.com>
2023-05-03 16:19:18 +0900

    Feat: item01 코드 보완
    상속에 대한 부분과 테스트코드에 대해서 코드를 보완함

--- EffectiveJavaStudy/src/main/java/me/timonpark/ejs/effectivejavastudy/item01/ElectricCar.java
+++ EffectiveJavaStudy/src/main/java/me/timonpark/ejs/effectivejavastudy/item01/ElectricCar.java
@@ -3,28 +3,22 @@ package me.timonpark.ejs.effectivejavastudy.item01;
 // 전기차 클래스 구현
 public class ElectricCar extends Car {
 
-    private String model;
-    private int year;
-    private String type;
-
     public ElectricCar(String model, int year) {
-        this.model = model;
-        this.year = year;
-        this.type = "electric";
+        super(model, year, "electric");
     }
 
     @Override
     public String getModel() {
-        return model;
+        return this.getModel();
     }
 
     @Override
     public int getYear() {
-        return year;
+        return this.getYear();
     }
 
     @Override
     public String getType() {
-        return type;
+        return this.getType();
     }
 }
--- EffectiveJavaStudy/src/test/java/me/timonpark/ejs/effectivejavastudy/Item01Tests.java
+++ EffectiveJavaStudy/src/test/java/me/timonpark/ejs/effectivejavastudy/Item01Tests.java
@@ -14,11 +14,22 @@ public class Item01Tests {
     void ConstructorTest(){
         // 생성자 방식
         Car electricCar = new ElectricCar("tesla", 1);
+        Car electricCar2 = new ElectricCar("tesla", 1);
 
         //정적 팩토리 메서드
-        Car electricCar2 = CarFactory.createElectricCar("tesla", 1);
+        Car electricCar3 = CarFactory.createElectricCar("tesla", 1);
 
+        //정적 팩토리 메서드
+        Car electricCar4 = CarFactory.createElectricCar("tesla", 1);
+
+        System.out.println(electricCar.hashCode()); // 2124360754
+        System.out.println(electricCar2.hashCode()); // 434167806
+        System.out.println(electricCar3.hashCode()); // 1565397985
+        System.out.println(electricCar4.hashCode()); // 1565397985
         // 객체 결과비교
-        assertThat(electricCar).isEqualTo(electricCar2);
+        assertThat(electricCar).isNotEqualTo(electricCar2);
+        assertThat(electricCar).isNotEqualTo(electricCar3);
+        assertThat(electricCar2).isNotEqualTo(electricCar3);
+        assertThat(electricCar3).isEqualTo(electricCar4);
     }
 }
